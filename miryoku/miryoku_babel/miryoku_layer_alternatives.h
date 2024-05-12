7e9b87e851c459aa3e9400d73dca2b348864db50
K. Andrew Parker <kparker@citytech.cuny.edu>
2024-05-12 13:14:31 -0400

    swap left and right thumbs

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -118,7 +118,7 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MOUSE, ESC),U_LT(U_NAV, SPACE),U_LT(U_NUM, TAB),  U_LT(U_MEDIA, RET),U_LT(U_SYM, BSPC), U_LT(U_FUN, MINUS),U_NP,              U_NP
+U_NP,              U_NP,              U_LT(U_MOUSE, ESC),U_LT(U_NAV, BSPC) ,U_LT(U_NUM, RET),  U_LT(U_MEDIA, TAB),U_LT(U_SYM, SPACE),U_LT(U_FUN, MINUS),U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTZ \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Z,             &kp U,             &kp I,             &kp O,             &kp P,             \
@@ -351,7 +351,7 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp ASTRK,         &kp N7,            &kp N8,            &kp N9,            U_NA,              \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp PLUS,          &kp N4,            &kp N5,            &kp N6,            U_NA,              \
 U_NA,              &kp RALT,          &u_to_U_MEDIA,     &u_to_U_NUM,       U_NA,              &kp N0,            &kp N1,            &kp N2,            &kp N3,            U_NA,              \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp TAB,           &kp DOT,           U_NP,              U_NP
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp TAB,           &kp RET,           &kp DOT,           U_NP,              U_NP
 
 
 #define MIRYOKU_ALTERNATIVES_SYM_FLIP \
